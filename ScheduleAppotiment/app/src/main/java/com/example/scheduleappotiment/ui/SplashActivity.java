11c777f3086ac0ec85a2c31a31eb3b17859cb9e0
siddharth9365 <30820602+siddharth9365@users.noreply.github.com>
2021-08-01 07:45:21 -0400

    Auto Login

--- ScheduleAppotiment/app/src/main/java/com/example/scheduleappotiment/ui/SplashActivity.java
+++ ScheduleAppotiment/app/src/main/java/com/example/scheduleappotiment/ui/SplashActivity.java
@@ -6,12 +6,17 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 
 import com.example.scheduleappotiment.BuildConfig;
+import com.example.scheduleappotiment.MainActivity;
 import com.example.scheduleappotiment.databinding.ActivitySplashBinding;
 import com.example.scheduleappotiment.utility.BaseActivity;
+import com.example.scheduleappotiment.utility.CommonUtility;
+import com.example.scheduleappotiment.utility.MySharedPref;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends BaseActivity {
 
     private ActivitySplashBinding mBinding;
+    private boolean needLogin=false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,13 +24,22 @@ public class SplashActivity extends BaseActivity {
         mBinding=ActivitySplashBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
         mBinding.versionTxt.setText(BuildConfig.VERSION_NAME);
+        init();
     }
 
+    private void init(){
+        needLogin= FirebaseAuth.getInstance().getCurrentUser() == null || CommonUtility.isEmpty(MySharedPref.getInstance(this).getContactId());
+    }
     @Override
     protected void onResume() {
         super.onResume();
         new Thread(()->{
-            Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+            Intent intent;
+            if (needLogin)intent=new Intent(SplashActivity.this,LoginActivity.class);
+            else{
+                intent=new Intent(SplashActivity.this, MainActivity.class);
+                intent.putExtra("uid",FirebaseAuth.getInstance().getUid());
+            }
             try{
                 Thread.sleep(3000);
             }catch (Exception e){
